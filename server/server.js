0aa32160f12b1e3ba0ab1ecb2b800688984ff007
saipranathi2296 <vpranathi9000@gmail.com>
2025-08-01 13:30:18 +0530

    Added home api runnign

--- server/server.js
+++ server/server.js
@@ -12,6 +12,10 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('API is running...');   
+});
+
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/dashboard', require('./routes/dashboard'));
